f2e7b142ac50db82a186480737afb0ed1d97757a
Brandon McAnsh <brandonm@matricom.net>
2014-05-26 10:12:21 -0400

    Hdmi: Cleanup resolution picker
    * Disable freescale for 720p and 1080p
    * Use SystemWriter solely instead of WindowManager
    
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- src/com/android/settings/HdmiSettings.java
+++ src/com/android/settings/HdmiSettings.java
@@ -41,37 +41,6 @@ public class HdmiSettings extends SettingsPreferenceFragment
     private static final String KEY_AUTO_SWITCH = "auto_switch";
     private static final String KEY_OUTPUT_MODE ="output_mode";
 
-    private static final String[] COMMON_MODE_VALUE_LIST =  {"720","1080"};
-    private final static String sel_720poutput_x = "ubootenv.var.720poutputx";
-    private final static String sel_720poutput_y = "ubootenv.var.720poutputy";
-    private final static String sel_720poutput_width = "ubootenv.var.720poutputwidth";
-    private final static String sel_720poutput_height = "ubootenv.var.720poutputheight";
-    private final static String sel_1080ioutput_x = "ubootenv.var.1080ioutputx";
-    private final static String sel_1080ioutput_y = "ubootenv.var.1080ioutputy";
-    private final static String sel_1080ioutput_width = "ubootenv.var.1080ioutputwidth";
-    private final static String sel_1080ioutput_height = "ubootenv.var.1080ioutputheight";
-    private final static String sel_1080poutput_x = "ubootenv.var.1080poutputx";
-    private final static String sel_1080poutput_y = "ubootenv.var.1080poutputy";
-    private final static String sel_1080poutput_width = "ubootenv.var.1080poutputwidth";
-    private final static String sel_1080poutput_height = "ubootenv.var.1080poutputheight";
-    private static final String ppscalerRectFile = "/sys/class/ppmgr/ppscaler_rect";
-    private static final String updateFreescaleFb0File = "/sys/class/graphics/fb0/update_freescale";
-    private static final String freescaleFb0File = "/sys/class/graphics/fb0/free_scale";
-    private static final String freescaleFb1File = "/sys/class/graphics/fb1/free_scale";
-    private static final String mHdmiPluggedVdac = "/sys/class/aml_mod/mod_off";
-    private static final String mHdmiUnpluggedVdac = "/sys/class/aml_mod/mod_on";
-    private static final String HDMI_SUPPORT_LIST_SYSFS = "/sys/class/amhdmitx/amhdmitx0/disp_cap";
-    private static final String ppscalerFile = "/sys/class/ppmgr/ppscaler";
-    private static final String videoAxisFile = "/sys/class/video/axis";
-    private static final String request2XScaleFile = "/sys/class/graphics/fb0/request2XScale";
-    private static final String scaleAxisOsd0File = "/sys/class/graphics/fb0/scale_axis";
-    private static final String scaleAxisOsd1File = "/sys/class/graphics/fb1/scale_axis";
-    private static final String scaleOsd1File = "/sys/class/graphics/fb1/scale";
-    private static final String outputModeFile = "/sys/class/display/mode";
-    private static final String outputAxisFile= "/sys/class/display/axis";
-    private static final String windowAxisFile = "/sys/class/graphics/fb0/window_axis";
-    private final static String DISPLAY_MODE_SYSFS = "/sys/class/display/mode";
-
     private static final int OUTPUT720_FULL_WIDTH = 1280;
     private static final int OUTPUT720_FULL_HEIGHT = 720;
     private static final int OUTPUT1080_FULL_WIDTH = 1920;
@@ -214,83 +183,42 @@ public class HdmiSettings extends SettingsPreferenceFragment
 
     }
 
-    public static int[] getPosition(SystemWriteManager sw, String mode) {
-        int[] curPosition = { 0, 0, OUTPUT720_FULL_WIDTH, OUTPUT720_FULL_HEIGHT };
-        int index = 0; // 720p
-        for (int i = 0; i < COMMON_MODE_VALUE_LIST.length; i++) {
-            if (mode.equalsIgnoreCase(COMMON_MODE_VALUE_LIST[i]))
-                index = i;
-        }
-        switch (index) {
-            case 0:
-                curPosition[0] = sw.getPropertyInt(sel_720poutput_x, 0);
-                curPosition[1] = sw.getPropertyInt(sel_720poutput_y, 0);
-                curPosition[2] = sw.getPropertyInt(sel_720poutput_width, OUTPUT720_FULL_WIDTH);
-                curPosition[3] = sw.getPropertyInt(sel_720poutput_height, OUTPUT720_FULL_HEIGHT);
-                break;
-            case 1:
-                curPosition[0] = sw.getPropertyInt(sel_1080poutput_x, 0);
-                curPosition[1] = sw.getPropertyInt(sel_1080poutput_y, 0);
-                curPosition[2] = sw.getPropertyInt(sel_1080poutput_width, OUTPUT1080_FULL_WIDTH);
-                curPosition[3] = sw.getPropertyInt(sel_1080poutput_height, OUTPUT1080_FULL_HEIGHT);
-                break;
-            default:
-                curPosition[0] = sw.getPropertyInt(sel_720poutput_x, 0);
-                curPosition[1] = sw.getPropertyInt(sel_720poutput_y, 0);
-                curPosition[2] = sw.getPropertyInt(sel_720poutput_width, OUTPUT720_FULL_WIDTH);
-                curPosition[3] = sw.getPropertyInt(sel_720poutput_height, OUTPUT720_FULL_HEIGHT);
-                break;
-        }
-        return curPosition;
+    public void disableFreescale(String mode) {
+        // default to 720p
+        int w = 1280;
+        int h = 720;
 
+        if (mode.contains("1080")) {
+            w = 1920;
+            h = 1080;
+        }
+        // turn off fb freescale
+        Utils.writeSysfs(sw, "/sys/class/graphics/fb0/freescale_mode", "0");
+        Utils.writeSysfs(sw, "/sys/class/graphics/fb1/freescale_mode", "0");
+        Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale_axis", "0 0 1279 719");
+
+        Utils.writeSysfs(sw, "/sys/class/ppmgr/ppscaler", "0");
+        Utils.writeSysfs(sw, "/sys/class/video/disable_video", "0");
+        // now default video display to off
+        Utils.writeSysfs(sw, "/sys/class/video/disable_video", "1");
+
+        // revert display axis
+        Utils.writeSysfs(sw, "/sys/class/display/axis", "0 0 " + String.valueOf(w - 1) + " " + String.valueOf(h - 1));
     }
 
-    public void updateSysfs(int mode) {
-        int[] curPosition = getPosition(sw, String.valueOf(mode));
-        String mWinAxis = curPosition[0]+ " " + curPosition[1]
-                + " " + (curPosition[0]+curPosition[2]-1)+ " " + (curPosition[1]+curPosition[3]-1);
-        String mVideoAxis = curPosition[0] + " " + curPosition[1]+ " " + (curPosition[2] + curPosition[0]-1)
-                + " " + (curPosition[3] + curPosition[1]-1);
-        String mDisplayAxis = curPosition[0] + " " + curPosition[1]
-                + Utils.getDisplayAxisByMode(mode) + curPosition[0] + " " + curPosition[1] + " " + 18 + " " + 18;
-
-
-        if (mode == OUTPUT1080_FULL_HEIGHT) {
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/freescale_mode", "1");
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale_axis", "0 0 1919 1079");
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/window_axis", mWinAxis);
-            if (curPosition[0] == 0 && curPosition[1] == 0) {
-                Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale","0");
-            } else {
-                Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale","0x10001");
-            }
-        } else if (mode == OUTPUT720_FULL_HEIGHT) {
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/freescale_mode","1");
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale_axis", "0 0 1279 719");
-            Utils.writeSysfs(sw, "/sys/class/graphics/fb0/window_axis", mWinAxis);
-            if (curPosition[0] == 0 && curPosition[1] == 0) {
-                Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale", "0");
-            } else {
-                Utils.writeSysfs(sw, "/sys/class/graphics/fb0/free_scale", "0x10001");
-            }
-        }
-        Utils.writeSysfs(sw,videoAxisFile, mVideoAxis);
-        Utils.writeSysfs(sw, outputAxisFile, mDisplayAxis);
+    public void setResolution(String mode) {
+        Utils.writeSysfs(sw, "/sys/class/display/mode", mode);
     }
+
+    public void updateModeProps(String newMode) {
+        sw.setProperty(COMMON_MODE_PROP, newMode);
+        sw.setProperty(HDMI_MODE_PROP, newMode);
+    }
+
      private void updateHdmiOutput(String newMode) {
-         int res = OUTPUT720_FULL_HEIGHT;
-         if (newMode.contains(String.valueOf(OUTPUT1080_FULL_HEIGHT))) {
-             res = OUTPUT1080_FULL_HEIGHT;
-             // Turn auto key off to allow 1080p smooth change
-             Settings.System.putInt(getContentResolver(), Settings.System.HDMI_AUTO_SWITCH, 0);
-         }
-         getPosition(sw, newMode);
-         setDensity(newMode);
-         setDisplaySize(newMode);
-
-         updateSysfs(res);
-         sw.setProperty(COMMON_MODE_PROP, newMode);
-         sw.setProperty(HDMI_MODE_PROP, newMode);
+         setResolution(newMode);
+         disableFreescale(newMode);
+         updateModeProps(newMode);
     }
 
     @Override
